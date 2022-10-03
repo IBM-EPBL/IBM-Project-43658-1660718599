bf265afd6832c2db06076d2060a5ce383a16daed
SpaRtaleon <raghubalan2972001@gmail.com>
2022-10-03 17:21:19 +0530

    second assignment

---
+++ Assignment/RAGHU BALAN Team Lead/Assignment 2/concatenate, reverse and slice a string.py
@@ -0,0 +1,8 @@
+fWord=input("first word : ")
+lWord=input("secoond word : ")
+sentence=fWord+" "+lWord
+print("\nsentence--",sentence)
+reverse=sentence[::-1]
+print("\nreverse--",reverse)
+sliced=sentence[0:4]
+print("\nslice--",sliced)
