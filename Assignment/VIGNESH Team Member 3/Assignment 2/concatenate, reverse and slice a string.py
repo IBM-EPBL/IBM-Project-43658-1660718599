87712d88775d5b9795fd8edfff03086f9d4959d1
SpaRtaleon <raghubalan2972001@gmail.com>
2022-10-16 01:00:14 +0530

    upload changes

--- Assignment/VIGNESH Team Member 3/Assignment 2/concatenate, reverse and slice a string.py
+++
@@ -1,8 +0,0 @@
-fWord=input("first word : ")
-lWord=input("secoond word : ")
-sentence=fWord+" "+lWord
-print("\nsentence--",sentence)
-reverse=sentence[::-1]
-print("\nreverse--",reverse)
-sliced=sentence[0:4]
-print("\nslice--",sliced)
